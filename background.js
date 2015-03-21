5c72241b398a1d81ac4b829f033fe87ce02d273d
Carson Lourenco <carson.loureno101@gmail.com>
2015-03-21 15:32:51 +1300

    Fixed listener issue

--- background.js
+++ background.js
@@ -1,24 +1,15 @@
-
- var selected = "";
  
- 
-
 function searchYoutube () {
 	chrome.tabs.executeScript(null, {file:"script.js"});
-	
-	chrome.extension.onRequest.addListener(search);
-
 }
 
 function search(request, sender, sendResponse) {
-		console.log(request.viewsource);
-		var selectedText = request.viewsource;
-		var urlString = "https://www.youtube.com/results?search_query=" + selectedText;
-		chrome.tabs.create({url: urlString});
-		console.log("executed");
-	
-	}
+	console.log(request.viewsource);
+	var selectedText = request.viewsource;
+	var urlString = "https://www.youtube.com/results?search_query=" + selectedText;
+	chrome.tabs.create({url: urlString});
+	console.log("executed");
+}
 
+chrome.extension.onRequest.addListener(search);
 chrome.contextMenus.create({title: "Search Youtube for selection", contexts:["selection"], onclick: searchYoutube});
-
-
