8952009a4bc33375e139367eb8824967470ccf28
Carson Lourenco <carson.loureno101@gmail.com>
2015-03-27 11:12:37 +1300

    Now shows selection in context menu

--- background.js
+++ background.js
@@ -5,11 +5,19 @@ function searchYoutube () {
 
 function search(request, sender, sendResponse) {
 	console.log(request.viewsource);
-	var selectedText = request.viewsource;
+	var selectedText = removeHTMLTags(request.viewsource);
 	var urlString = "https://www.youtube.com/results?search_query=" + selectedText;
 	chrome.tabs.create({url: urlString});
 	console.log("executed");
+	
+}
+
+function removeHTMLTags(string){
+	var div = document.createElement("div");
+	div.innerHTML = string;
+	console.log(div.innerText);
+	return div.innerText;
 }
 
 chrome.extension.onRequest.addListener(search);
-chrome.contextMenus.create({title: "Search Youtube for selection", contexts:["selection"], onclick: searchYoutube});
+chrome.contextMenus.create({title: "Search Youtube for \'%s\'", contexts:["selection"], onclick: searchYoutube});
